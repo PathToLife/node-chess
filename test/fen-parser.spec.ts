05ed6459807c6ffc8a80a570d274614d16a9a828
Sean Z <contact@seanzeng.com>
2020-04-30 20:17:40 +1200

    fen test

--- test/fen-parser.spec.ts
+++ test/fen-parser.spec.ts
@@ -2,20 +2,22 @@ import classicEngine from '../src/engine/instances/classic/engine';
 import {expect} from 'chai';
 
 describe("fen parsing tests", () => {
-	// Build a basic board with a pawn a valid piece
-	const classicEngine1 = classicEngine();
 
-	// Initialise the board with the default string position
-	classicEngine1.parseFenString();
+    it("will have a pawn at 7,2", () => {
+        const rankEight = classicEngine().boardState.ranks[7];
+        const fileOne = rankEight.squares[2];
+        expect(fileOne.piece).to.exist;
+    });
 
-	it("will have a pawn at 7,2", () => {
-		const rankEight = classicEngine1.boardState.ranks[7];
-		const fileOne = rankEight.squares[2];
-		expect(fileOne.piece).to.exist;
-	});
+    it("will generate default classic chess fen string", () => {
+        const s = classicEngine().stringifyFenString()
+        expect(s).to.equal('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
+    })
 
-	it("will generate default classic chess fen string", () => {
-		const s = classicEngine1.stringifyFenString()
-		expect(s).to.equal('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
-	})
+    it("will generate correct fen string, pawn move", () => {
+        const e = classicEngine();
+        e.movePiece({from: {file: 1, rank: 2}, to: {file: 1, rank: 3}})
+		const s = e.stringifyFenString()
+        expect(s).to.equal('rnbqkbnr/pppppppp/8/8/8/P7/1PPPPPPP/RNBQKBNR b KQkq - 0 2')
+    })
 });
