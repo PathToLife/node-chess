72e114cf4b248a71f47a62254d2af16d331537d0
Sean Z <contact@seanzeng.com>
2020-05-02 14:52:10 +1200

    remove borked version from package.json test

--- test/setup.spec.ts
+++
@@ -1,9 +0,0 @@
-import {version} from '../src/index';
-import {expect} from 'chai';
-
-describe('should import version', () => {
-    it('should have version', function () {
-        expect(version).to.exist;
-        console.log(version);
-    });
-})
